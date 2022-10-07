59b33e5563078b0f02192fea9eff24cac23524b2
Crevan <a.siverskii@gmail.com>
2022-10-07 18:25:56 +0300

    [add; stub] WorldObject.java

---
+++ src/com/l2j/emu/gameserver/model/WorldObject.java
@@ -0,0 +1,19 @@
+package com.l2j.emu.gameserver.model;
+
+//TODO STUB
+public class WorldObject {
+    public int getX() {
+        //TODO
+        return 0;
+    }
+
+    public int getY() {
+        //TODO
+        return 0;
+    }
+
+    public double getZ() {
+        //TODO
+        return 0.0;
+    }
+}
