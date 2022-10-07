bcaf8f33e69420063f311b26ce891a7b105fd088
Crevan <a.siverskii@gmail.com>
2022-10-07 18:25:06 +0300

    [add; stub] Creature.java

---
+++ src/com/l2j/emu/gameserver/model/actor/Creature.java
@@ -0,0 +1,11 @@
+package com.l2j.emu.gameserver.model.actor;
+
+import com.l2j.emu.gameserver.model.WorldObject;
+
+//TODO STUB
+public class Creature extends WorldObject {
+    public double getCollisionRadius() {
+        //TODO STUB
+        return 0.0;
+    }
+}
