4b696844805d36e103cd43d5c89d38f0509766d1
Crevan <a.siverskii@gmail.com>
2022-10-07 18:25:22 +0300

    [add; stub] Point2D.java

---
+++ src/com/l2j/emu/gameserver/model/location/Point2D.java
@@ -0,0 +1,19 @@
+package com.l2j.emu.gameserver.model.location;
+
+//TODO STUB
+public class Point2D {
+    protected int x;
+    protected int y;
+
+    public Point2D(int x, int y) {
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
