7788724e4bb9994a93d12b1b6a386d03e28817f9
Crevan <a.siverskii@gmail.com>
2022-10-16 19:44:09 +0200

    [add, stub] SelectorThread.java add stub for methods

--- src/com/l2j/emu/commons/mmocore/SelectorThread.java
+++ src/com/l2j/emu/commons/mmocore/SelectorThread.java
@@ -1,5 +1,19 @@
 package com.l2j.emu.commons.mmocore;
 
+import java.nio.ByteBuffer;
+
 //TODO STUB
 public final class SelectorThread<T extends MMOClient<?>> extends Thread {
+    public void recycleBuffer(ByteBuffer byteBuffer) {
+        //TODO
+    }
+
+    public void closeConnection(MMOConnection<T> connection) {
+        //TODO
+    }
+
+    public ByteBuffer getPooledBuffer() {
+        //TODO
+        return null;
+    }
 }
