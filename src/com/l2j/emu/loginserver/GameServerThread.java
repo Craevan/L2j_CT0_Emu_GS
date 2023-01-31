52b92901009b37b2d27c7f3cc1808096f65faebc
Crevan <a.siverskii@gmail.com>
2023-01-31 22:13:49 +0100

    [stub] GameServerThread

---
+++ src/com/l2j/emu/loginserver/GameServerThread.java
@@ -0,0 +1,12 @@
+package com.l2j.emu.loginserver;
+
+import java.net.Socket;
+
+public class GameServerThread extends Thread {
+    //todo
+
+
+    public GameServerThread(Socket socket) {
+
+    }
+}
